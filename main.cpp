089bbc9eb812ec280ba8c9940f83616ac351998a
Rafael Ramos <22090@portalcedi.edu.mx>
2024-05-20 13:53:14 -0600

    Pelotita movil

--- main.cpp
+++ main.cpp
@@ -5,9 +5,14 @@ using namespace sf;
 
 int main()
 {
-    RenderWindow window(VideoMode(200, 200), "SFML works!");
-    CircleShape shape(100.f);
+    RenderWindow window(VideoMode(800, 600), "SFML works!");
+    window.setFramerateLimit(60);
+    CircleShape shape(10.f); // Reducí el tamaño del círculo para que sea más visible en la ventana pequeña
     shape.setFillColor(Color::Green);
+
+    // Posición inicial del personaje
+    Vector2f position(100.f, 100.f);
+    float speed = 5.0f; // Velocidad de movimiento del personaje
 
     while (window.isOpen())
     {
@@ -17,6 +22,29 @@ int main()
             if (event.type == Event::Closed)
                 window.close();
         }
+
+        // Movimiento del personaje
+        if (Keyboard::isKeyPressed(Keyboard::Up))
+            position.y -= speed;
+        if (Keyboard::isKeyPressed(Keyboard::Down))
+            position.y += speed;
+        if (Keyboard::isKeyPressed(Keyboard::Left))
+            position.x -= speed;
+        if (Keyboard::isKeyPressed(Keyboard::Right))
+            position.x += speed;
+
+        // Limitar la posición del personaje para que no salga de la ventana
+        if (position.x < 0)
+            position.x = 0;
+        if (position.y < 0)
+            position.y = 0;
+        if (position.x > window.getSize().x - shape.getRadius() * 2) // Resta el radio del círculo para ajustar el límite derecho
+            position.x = window.getSize().x - shape.getRadius() * 2;
+        if (position.y > window.getSize().y - shape.getRadius() * 2) // Resta el radio del círculo para ajustar el límite inferior
+            position.y = window.getSize().y - shape.getRadius() * 2;
+
+        // Establecer la posición del personaje
+        shape.setPosition(position);
 
         window.clear();
         window.draw(shape);
